6f534772092da0e67383605f99a17914c24b35fc
SwapnilGopale <swapnilg683@gmail.com>
2022-04-07 01:13:11 +0530

    PL-02 - Create parking functionality: Changes to display empty slots

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const eleCreateParkingLot = document.getElementById('btn-create');
 var parkingLot;
+const eleCreateParkingLot = document.getElementById('btn-create');
+const eleParkNewCar = document.getElementById('btn-park');
 eleCreateParkingLot.addEventListener('click',()=>{
     const parkingLotSize = prompt("Please enter parking lot size");
     if(parkingLotSize && !isNaN(parkingLotSize)){
@@ -7,8 +8,18 @@ eleCreateParkingLot.addEventListener('click',()=>{
     }
 })
 
+eleParkNewCar.addEventListener('click',()=>{
+    if(!parkingLot) return;
+})
+
 function createNewParkingLot(parkingLotSize){
-    parkingLot = Array(+parkingLotSize).fill('empty');
+    parkingLot = Array(+parkingLotSize).fill({occupied:'empty',car_reg_num:'',car_color:'',ticket_number:''});
     eleCreateParkingLot.style.display = 'none';
     document.getElementById('no-lots').style.display = 'none';
+    displayEmptySlots();
+}
+
+function displayEmptySlots(){
+    const parkingLotArea =  document.getElementById('parking-lot');
+    parkingLot.forEach((element,index)=>parkingLotArea.innerHTML+=`<div class="parking-slot" style="background = ${element.color}" data-index=${index}><span>${element.occupied}</span></div>`);
 }
